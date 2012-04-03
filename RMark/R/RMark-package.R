d78e7dd0d79706763dcf90c5854e9e841d5ae00f
Jeff Laake <jefflaake@gmail.com>
2012-04-03 16:48:41 -0700

    Added NEWS file which starts at v2.1.0; older material is still in Whatsnew help file.

--- RMark/R/RMark-package.R
+++ RMark/R/RMark-package.R
@@ -2012,28 +2012,10 @@ NULL
 
 #' Summary of changes by version
 #' 
-#' A good place to look for new changes.  Often I'll add changes here but don't
+#' A good place to look for changes.  Often I'll add changes here but don't
 #' always get to it in the documentation for awhile.  They are ordered from
 #' newest to oldest.
 #' 
-#' #' Version 2.1.1 (3 April 2012) \itemize{
-#' \item Fixed bug in  \code{\link{make.mark.model}} for mlogit specification with pi and Omega parameters in RDMSMisClass models 
-#' with more than 2 observable strata.  Thanks to Claudia Penaloza for reporting this problem.
-#' \item Modified values in parameters.txt to enable model RDHFHet to work. Thanks to Darryl McKenzie for reporting this
-#' problem.
-#' }
-#' 
-#' Version 2.1.0 (27 Jan 2012) \itemize{ \item Patch was made to \code{\link{run.mark.model}} so that
-#' a filename can be specified to pick up orphaned MARK output files for a model that was run but not saved. This used
-#' to work but when I added code to allow user-specified prefixes (ie something other than marknnn) the change I made broke
-#' the other feature.  Thanks to Kelly Hastings for bringing this to my attention.
-#' \item Patch was made to  \code{\link{make.mark.model}}to allow design data to be deleted on shared parameters. Thanks to 
-#' Claudia Penaloza for reporting this issue.
-#' \item In \code{\link{process.data}} the restriction of having only one non-unobservable state for robust designs was
-#'  removed but a note is still issued when this occurs. Also, a warning is issued if you specify more than one value in the vector for 
-#'  initial.ages but an age variable is not identified in the groups. Also, allowed "." in Royle count occupancy models.
-#' \item An argument run was added to \code{\link{mark}} which if set to FALSE will not run the model after it is created. 
-#' }
 #' Version 2.0.9 (1 Dec 2011) \itemize{ \item Patch was made to  \code{\link{make.mark.model}} to fix bug in
 #' PIM creation for a multi-session model and there was just 1 session. Thanks to Erin Roche for helping to 
 #' identify this bug.

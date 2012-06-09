16505fbfdfc59b41499076cd69c62fa00a4fbe73
Jeff Laake <jefflaake@gmail.com>
2012-06-08 20:04:10 -0700

    reset accumulate to true for v2.1.3

--- RMark/R/make.mark.model.R
+++ RMark/R/make.mark.model.R
@@ -1131,7 +1131,7 @@ create.agenest.var=function(data,init.agevar,time.intervals)
 	    zzd=data.frame(cbind(zz,data$data[,covariates]))
   else
 	    zzd=zz
-  accumulate=FALSE
+  accumulate=TRUE
   if(etype!="Nest" & accumulate)
   {
 	  pasted.data=apply(zzd, 1, paste, collapse = "")
